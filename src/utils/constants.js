0ccdcb1fd2a876d1560d764446210622a8149c32
mrbabu07 <mdjahedulislamjaved@gmail.com>
2026-05-09 15:01:39 +0600

    feat: add application constants for consistent data

---
+++ src/utils/constants.js
@@ -0,0 +1,44 @@
+// Application constants
+
+export const APP_NAME = 'Rise at Seven Clone'
+export const APP_VERSION = '1.0.0'
+export const APP_DESCRIPTION = 'A pixel-perfect clone of the Rise at Seven agency website'
+
+export const SOCIAL_LINKS = {
+  TWITTER: 'https://twitter.com/riseatseven',
+  LINKEDIN: 'https://www.linkedin.com/company/rise-at-seven',
+  INSTAGRAM: 'https://www.instagram.com/riseatseven',
+  YOUTUBE: 'https://www.youtube.com/@riseatseven',
+  TIKTOK: 'https://www.tiktok.com/@riseatseven',
+  FACEBOOK: 'https://www.facebook.com/riseatseven'
+}
+
+export const CONTACT_INFO = {
+  EMAIL: 'hello@riseatse7en.com',
+  PHONE: '+44 114 272 4700',
+  ADDRESS: 'Sheffield, UK'
+}
+
+export const NAVIGATION_ITEMS = [
+  { label: 'Services', href: '/services' },
+  { label: 'Work', href: '/work' },
+  { label: 'About', href: '/about' },
+  { label: 'Blog', href: '/blog' },
+  { label: 'Contact', href: '/contact' }
+]
+
+export const SERVICES = [
+  'SEO',
+  'Content Marketing',
+  'Social Media',
+  'Digital PR',
+  'Data & Analytics',
+  'Strategy'
+]
+
+export const AWARDS = [
+  'Global Search Awards',
+  'Drum Awards',
+  'UK Social Media Awards',
+  'Content Marketing Awards'
+]
